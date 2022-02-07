fa2f50183677bd29edada3fbfaec8ea07c883d9e
Saurin Thakkar <saurin.thakkar13@gmail.com>
2022-02-07 23:08:33 +0530

    removed all template files

---
+++ reactapp/src/user.js
@@ -0,0 +1,11 @@
+function User() {
+  return (
+    <div>
+      <h1>Hey my name is saurin</h1>
+      <p>I am a web developer</p>
+      <p>I will love writing</p>
+    </div>
+  );
+}
+
+export default User;

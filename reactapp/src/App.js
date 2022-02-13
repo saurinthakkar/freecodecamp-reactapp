b256e3566d6bce82310f7229792099bc9034ba31
Saurin Thakkar <saurin.thakkar13@gmail.com>
2022-02-14 00:06:02 +0530

    added custom hook

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,11 +1,26 @@
 //import logo from "./logo.svg";
+//import { useEffect, useState } from "react";
 import "./App.css";
-import User from "./user";
+import Users from "./Users";
+//import User from "./user";
 
 function App() {
+  // const [count, setCount] = useState(0);
+  // // const changeName = () => {
+  // //   setName("Thakkar");
+  // // };
+  // useEffect(() => {
+  //   console.log(`you have clicked the button1 ${count} times`);
+  // }, [count]);
+
+  // const [count2, setCount2] = useState(0);
+
+  // useEffect(() => {
+  //   console.log(`you have clicked the button2 ${count2} times`);
+  // }, [count2]);
   return (
     <div className="App">
-      <User />
+      <Users />
     </div>
   );
 }
